4b5782693dfaec6cd5c3174f4c9c0f4bfbefdd58
nowakowski-michal <michal121999@gmail.com>
2024-04-25 13:30:24 +0200

    Dodano zmiany z Jenkinsa

--- ProjectTests/obj/Debug/net6.0/ProjectTests.AssemblyInfo.cs
+++ ProjectTests/obj/Debug/net6.0/ProjectTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3232144d4ff6bc632ef04378d8e116def79c9bc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870f7569094272aa9e6f65bdd942fb851769120d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
